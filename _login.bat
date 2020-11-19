0180e8c8e1abc69f07c8c1e1a26d5309c64f60f2
Marco Noel <Marco.Noel@ca.ibm.com>
2020-11-19 10:31:56 -0500

    Update _login.bat
    Removed credentials and API keys

--- _login.bat
+++ _login.bat
@@ -1,7 +1,7 @@
-ibmcloud login -a https://api.us-east.bluemix.net --apikey RK9yv5NIpJcooltObCgtcyR6NM0Yw52WO00IMqBp1dkE
+ibmcloud login -a https://api.us-east.bluemix.net --apikey <key>>
 ibmcloud cs region-set us-east
 ibmcloud target -o sttorg -s sttspace
-bx login -a https://api.us-east.bluemix.net -apikey RK9yv5NIpJcooltObCgtcyR6NM0Yw52WO00IMqBp1dkE
+bx login -a https://api.us-east.bluemix.net -apikey <key>>
 bx target -o sttorg -s sttspace
 REM export KUBECONFIG=/Users/afaisman/.bluemix/plugins/container-service/clusters/stt_frontend_cluster/kube-config-wdc07-stt_frontend_cluster.yml
 REM [Environment]::SetEnvironmentVariable("KUBECONFIG", "C:\Users\AlexanderFaisman\.bluemix\plugins\container-service\clusters\stt_frontend_cluster\kube-config-wdc07-stt_frontend_cluster.yml", [EnvironmentVariableTarget]::User)
